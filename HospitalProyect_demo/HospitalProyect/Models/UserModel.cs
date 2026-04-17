1579d3a28e9dc8a870fe48a9fa659fc06264d7b5
Shadow01xd <you@example.com>
2026-04-16 22:14:49 -0600

    Practica 7

---
+++ HospitalProyect_demo/HospitalProyect/Models/UserModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HospitalProyect.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress(ErrorMessage = "formato de correo invalido")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+    }
+}
